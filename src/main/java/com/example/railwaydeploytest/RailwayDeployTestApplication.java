456a59e8066d2d247f795b8ac78e2b2f85257e84
Mateusz Wręczycki <setsudan.hana@gmail.com>
2023-04-22 20:42:40 +0200

    publish initial test application

---
+++ src/main/java/com/example/railwaydeploytest/RailwayDeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.railwaydeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayDeployTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayDeployTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/railwaydeploytest/RailwayDeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railwaydeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayDeployTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
